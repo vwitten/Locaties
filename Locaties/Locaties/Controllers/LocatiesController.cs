75dd0431a064e68d7ffadfc5869fbc757bde4996
vincent witten <vincent witten>
2017-02-06 14:35:19 +0100

    alle locaties worden met een marker weergegeven op een google maps. De Locatie class is een beetje aangepast zodat nu wel alle velden worden gevuld vanuit het JSON bestand. de navigatiebalk is aangepast voor betere navigatie. De code is gecommentaieerd.

--- Locaties/Locaties/Controllers/LocatiesController.cs
+++ Locaties/Locaties/Controllers/LocatiesController.cs
@@ -18,105 +18,24 @@ namespace Locaties.Controllers
         // GET: Locaties
         public ActionResult Index()
         {
+            //leest het JSON bestand in en slaat het op in een string.
             string json = System.IO.File.ReadAllText(Server.MapPath("~/JSON/locaties.json"));
+            //maakt een variable met alle locaties als lijst.
             var LocatieLijst = JsonConvert.DeserializeObject<List<Locatie>>(json);
-            return View(LocatieLijst/*db.Locaties.ToList()*/);
+            //geeft een webpagina weer met de locatie variable als lijst.
+            return View(LocatieLijst);
         }
 
-        // GET: Locaties/Details/5
-        public ActionResult Details(Guid? id)
+        public ActionResult GMap()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Locatie locatie = db.Locaties.Find(id);
-            if (locatie == null)
-            {
-                return HttpNotFound();
-            }
-            return View(locatie);
-        }
-
-        // GET: Locaties/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Locaties/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "LocatieID,Longitude,Street,Latitude,City,ZipCode,StoreName")] Locatie locatie)
-        {
-            if (ModelState.IsValid)
-            {
-                locatie.LocatieID = Guid.NewGuid();
-                db.Locaties.Add(locatie);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(locatie);
-        }
-
-        // GET: Locaties/Edit/5
-        public ActionResult Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Locatie locatie = db.Locaties.Find(id);
-            if (locatie == null)
-            {
-                return HttpNotFound();
-            }
-            return View(locatie);
-        }
-
-        // POST: Locaties/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "LocatieID,Longitude,Street,Latitude,City,ZipCode,StoreName")] Locatie locatie)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(locatie).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(locatie);
-        }
-
-        // GET: Locaties/Delete/5
-        public ActionResult Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Locatie locatie = db.Locaties.Find(id);
-            if (locatie == null)
-            {
-                return HttpNotFound();
-            }
-            return View(locatie);
-        }
-
-        // POST: Locaties/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(Guid id)
-        {
-            Locatie locatie = db.Locaties.Find(id);
-            db.Locaties.Remove(locatie);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            //leest het JSON bestand in en slaat het op in een string.
+            string json = System.IO.File.ReadAllText(Server.MapPath("~/JSON/locaties.json"));
+            //maakt een variable met alle locaties als lijst.
+            var LocatieLijst = JsonConvert.DeserializeObject<List<Locatie>>(json);
+            //hier wordt van de lijst met locaties een IEnumerable gemaakt.
+            IEnumerable<Locatie> LocatiesMarkers = LocatieLijst;
+            //geeft een webpagina weer met de locaties aangegeven op een Google map.
+            return this.View(LocatiesMarkers);
         }
 
         protected override void Dispose(bool disposing)
--- Locaties/Locaties/Models/Locatie.cs
+++ Locaties/Locaties/Models/Locatie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,13 +8,28 @@ namespace Locaties.Models
 {
     public class Locatie
     {
+        //unique code voor de locatie
         public Guid LocatieID { get; set; }
+        //de longitude van de locatie
+        [JsonProperty(PropertyName = "LONGITUDE")]
         public decimal Longitude { get; set; }
+        //de latitude van de locatie
+        [JsonProperty(PropertyName = "LATITUDE")]
         public decimal Latitude { get; set; }
+        //de straatnaam van de locatie
+        [JsonProperty(PropertyName = "STREET")]
         public string Street { get; set; }
+        //de stad naam van de locatie
+        [JsonProperty(PropertyName = "CITY")]
         public string City { get; set; }
+        //de zipcode van de locatie
+        [JsonProperty(PropertyName = "ZIP CODE")]
         public string ZipCode { get; set; }
+        //de winkelnaam van de locatie
+        [JsonProperty(PropertyName = "STORE NAME")]
         public string StoreName { get; set; }
-        private int? facebookID;
+        //het facebookID van de locatie
+        [JsonProperty(PropertyName = "FACEBOOK ID")]
+        public string FacebookID { get; set; }
     }
 }
