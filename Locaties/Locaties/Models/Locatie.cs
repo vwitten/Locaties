aa487b3a1c7be525a7785215fd3fbd001103c227
vincent witten <vincent witten>
2017-02-06 10:16:55 +0100

    JSON file met locaties toegevoegd en een model gemaakt voor de locaties

---
+++ Locaties/Locaties/Models/Locatie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locaties.Models
+{
+    public class Locatie
+    {
+        public decimal Longitude { get; set; }
+        public string Street { get; set; }
+        public decimal Latitude { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string StoreName { get; set; }
+        private int? facebookID;
+    }
+}
